95e788dc9b02b952cf922e17c77a87e239f9e6ca
Nick Pelone <npelone@unca.edu>
2014-01-24 14:04:57 -0500

    in-class work 1/24

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+/*jslint browser:true, devel:true */
 (function () {
     "use strict";
     //code
+    console.log("I dont actually do anything. Go away.");
+    var colorChanger = setInterval(function () {
+        var color = '#' + Math.floor(Math.random() * 16777215).toString(16), header = document.getElementById("header");
+        header.style.color = color;
+        console.log(color);
+    }, 100);
+    console.log("hello");
 }());
