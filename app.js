5f3872f09fa28b4b7803bc925d84715e44379bbe
Nick Pelone <npelone@unca.edu>
2014-01-31 13:57:01 -0500

    In class work 1/31
    this can crash brackets

--- app.js
+++ app.js
@@ -4,9 +4,14 @@
     //code
     console.log("I dont actually do anything. Go away.");
     var colorChanger = setInterval(function () {
-        var color = '#' + Math.floor(Math.random() * 16777215).toString(16), header = document.getElementById("header");
-        header.style.color = color;
-        console.log(color);
-    }, 100);
+        var color = '#' + Math.floor(Math.random() * 16777215).toString(16),
+            header = document.getElementsByClassName("ascii");
+        //console.log(header);
+        //deal with array
+        for (var i = 0; i < header.length; ++i) {
+            header[i].style.color = color;
+        }
+        //console.log(color);
+    }, 300);
     console.log("hello");
 }());
