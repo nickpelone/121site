1a69980e0d6e7ba7bdf951abfe3890d9a301190f
Nick Pelone <npelone@unca.edu>
2014-02-05 13:09:39 -0500

    Document random color

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 (function () {
     "use strict";
     //code
-    console.log("I dont actually do anything. Go away.");
     var colorChanger = setInterval(function () {
-        var color = '#' + Math.floor(Math.random() * 16777215).toString(16),
+        /* how this works:
+        We package up a string that is a valid hex color.
+        We start the string with the pound-sign,
+        then ask the Math object for the largest number that is less than or equal to
+        a random number, multiply it by white (0xFFFFFF), and then add a trailing zero to the number in hex (via bitwise shifts).
+        We then turn this number into a string and add it to the string we were working on earlier, with the pound sign.
+        */
+        var color = '#' + Math.floor(Math.random() * 0xFFFFFF << 0).toString(16),
             header = document.getElementsByClassName("ascii");
         //console.log(header);
         //deal with array
